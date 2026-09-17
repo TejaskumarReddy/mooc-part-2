3ea63dd9ed391a4640fc796ebe0cbbcb3a2ca669
TejaskumarReddy <tejaskumarreddy237@gmail.com>
2026-09-17 17:08:46 +0530

    Add files via upload

---
+++ part02-02_number_of_characters/src/number_of_characters.py
@@ -0,0 +1,6 @@
+# Write your solution here
+word=input("Please type in a word:")
+words = len(word)
+if words > 1:
+    print(f"There are {words} letters in the word {word}")
+print("Thank you!")
---
+++ part02-04_age_of_maturity/src/age_of_maturity.py
@@ -0,0 +1,6 @@
+# Write your solution here
+age=int(input("What is your age?"))
+if age >= 18:
+    print("You are of age!")
+else:
+    print("You are not of age!")
